d4b64c7da0e108ae11a0f53b1cb240d186b6ce45
Emrah Sariboz <emrahsariboz@Emrahs-MacBook-Pro.local>
2022-04-17 22:42:00 -0600

    VRF V2

--- contracts/Contract.sol
+++ contracts/Contract.sol
@@ -7,30 +7,19 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
 import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
 
-//import "@chainlink/contracts/src/v0.8/interfaces/LinkTokenInterface.sol";
-
-// Chainlink V2 --> Subscription ID is 2882!
-
-// interface Ichainlink {
-//     function transferAndCall(
-//         address to,
-//         uint256 value,
-//         bytes calldata data
-//     ) external returns (bool success);
-// }
-
 contract AdvancedCollectible is VRFConsumerBaseV2, ERC721 {
     // An interface used to create subscription, etc.
     VRFCoordinatorV2Interface COORDINATOR;
 
     uint256[] public s_randomWords;
+    uint256 public randomNum;
     address public owner;
 
     uint64 public s_subscriptionId;
     uint256 public s_requestId;
     uint16 requestConfirmations = 3;
     uint32 callbackGasLimit = 100000;
-    uint32 numWords = 1;
+    uint32 numWords = 2;
 
     //address vrfCoordinator = 0x6168499c0cFfCaCD319c818142124B7A15E857ab;
     //bytes32 keyHash =0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc;
@@ -63,26 +52,14 @@ contract AdvancedCollectible is VRFConsumerBaseV2, ERC721 {
         uint256[] memory randomWords
     ) internal override {
         s_randomWords = randomWords;
+        randomNum = s_randomWords[0];
     }
 
     function subscribe() external returns (uint64 subId) {
         s_subscriptionId = COORDINATOR.createSubscription();
     }
 
-    function addUser() external {
-        COORDINATOR.addConsumer(s_subscriptionId, owner);
+    function addUser(address _sc) external {
+        COORDINATOR.addConsumer(s_subscriptionId, _sc);
     }
-
-    // function fundSubscription(
-    //     address _linkAddress,
-    //     address _to,
-    //     uint256 _value,
-    //     bytes calldata _data
-    // ) external {
-    //     Ichainlink(_linkAddress).transferAndCall(
-    //         _to,
-    //         _value,
-    //         abi.encode(_data)
-    //     );
-    // }
 }
--- scripts/create_and_deploy.py
+++ scripts/create_and_deploy.py
@@ -7,20 +7,20 @@ from brownie import (
 )
 
 from scripts.helpful_scripts import get_account
-
-
 import time
 from web3 import Web3
+import os
 
 
 def main():
     link_token_address = "0x01BE23585060835E02B77ef475b0Cc51aA1e0709"
-
+    vrf_coordinator = "0x6168499c0cFfCaCD319c818142124B7A15E857ab"
+    keyHash = "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc"
     account = get_account()
 
     contract = AdvancedCollectible.deploy(
-        "0x6168499c0cFfCaCD319c818142124B7A15E857ab",
-        "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc",
+        vrf_coordinator,
+        keyHash,
         {"from": account},
     )
 
@@ -38,18 +38,26 @@ def main():
     link_token_contract = interface.LinkTokenInterface(link_token_address)
 
     tx = link_token_contract.transferAndCall(
-        "0x6168499c0cFfCaCD319c818142124B7A15E857ab",
-        50000000000000000,
+        vrf_coordinator,
+        2000000000000000000,
         convert.to_bytes(contract.s_subscriptionId()),
         {"from": account},
     )
 
     tx.wait(1)
 
+    # CHECK IF ENOUGH FUND!!
+    vrf_coordinator_contract = interface.VRFCoordinatorV2Interface(vrf_coordinator)
+
+    (balance, _, _, _) = vrf_coordinator_contract.getSubscription(
+        contract.s_subscriptionId()
+    )
+
+    print("Contract balance is ", balance)
+    print("x" * 50)
     print("Sub ID", contract.s_subscriptionId())
 
-    # Add owner
-    tx = contract.addUser({"from": account})
+    tx = contract.addUser(contract.address, {"from": account})
 
     tx.wait(1)
 
@@ -58,6 +66,6 @@ def main():
 
     print("Waiting for the callback...")
 
-    time.sleep(60)
+    time.sleep(70)
 
-    print(f"The random number is {contract.s_randomWords[0]}")
+    print(f"The random number is {contract.randomNum()}")
